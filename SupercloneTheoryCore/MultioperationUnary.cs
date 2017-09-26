feeb5665f3f8d31ee060e176acfabd44c5b00407
Sasha Malina <ef1rspb@gmail.com>
2017-09-26 22:13:34 +0300

    Installed NUnit & added stub test for Multioperation class

--- SupercloneTheoryCore/MultioperationUnary.cs
+++ SupercloneTheoryCore/MultioperationUnary.cs
@@ -3,14 +3,11 @@ namespace SupercloneTheoryCore
 {
 	public class MultioperationUnary : Multioperation
 	{
-		protected int _value;
-
-		public int Value { get { return _value; } }
 		public MultioperationUnary()
 		{
 		}
 
-		public MultioperationUnary(int val)
+        public MultioperationUnary(int decimalRepresentation) : base(decimalRepresentation)
 		{
 
 		}
---
+++ SupercloneTheoryCore/Tests/MultioperationTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SupercloneTheoryCore.Tests
+{
+    [TestFixture]
+    public class MultioperationTests
+    {
+        public MultioperationTests()
+        {
+        }
+
+        [Test]
+        public void MultioperationConstructorTest()
+        {
+            var multioperationUnary = new MultioperationUnary(10);
+
+            Assert.AreEqual(10, multioperationUnary.DecimalRepresentation);
+        }
+    }
+}
